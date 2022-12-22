455c87d1bfbd4edf7cb695996cbb94c98a88c6c5
Viktoriia212022 <109000671+Viktoriia212022@users.noreply.github.com>
2022-12-22 19:30:46 +0000

    Deploying to gh-pages from @ Viktoriia212022/goit-js-hw-09@cb838970857181eb3ca99f6b89c9fa4912324f81 🚀

---
+++ 01-color-switcher.07ac295a.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled","true"),e.removeAttribute("disabled");setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled","true"),clearInterval(timer)}))}();
+//# sourceMappingURL=01-color-switcher.07ac295a.js.map
